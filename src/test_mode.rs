e7f96ada239630c19b552134ffe7b61d2172ffb9
xuanzhen <xuanz20@mails.tsinghua.edu.cn>
2023-07-05 00:41:10 +0800

    basic_test_mode

---
+++ src/test_mode.rs
@@ -0,0 +1,60 @@
+use crate::utils::*;
+
+pub fn run() {
+    let ans_str: String = text_io::read!();
+    let answer = str2arr(&ans_str);
+    let ans_times = count_times(&answer);
+    let mut status = vec![Status::X; 26];
+    let mut times = 1;
+    while times <= 6 { // each guess
+        let mut success = true;
+        let mut result = vec![Status::X; 5];
+        let mut guess = String::new();
+        std::io::stdin().read_line(&mut guess).expect("INPUT ERROR");
+        guess.pop();
+        if !valid(&guess) {
+            println!("INVALID");
+            continue;
+        }
+        let guess = str2arr(&guess);
+        let mut guess_times = [0; 26];
+        for i in 0..5usize { // check for each char
+            let cha = guess[i];
+            guess_times[cha] += 1;
+            if cha == answer[i] {
+                result[i] = Status::G;
+                status[cha].update(Status::G);
+                // update the previous status
+                
+            } else {
+                success = false;
+                if guess_times[cha] <= ans_times[cha] {
+                    result[i] = Status::Y;
+                    status[cha].update(Status::Y);
+                } else {
+                    result[i] = Status::R;
+                    status[cha].update(Status::R);
+                }
+            }
+        }
+        // print the guess result
+        result.iter().for_each(|x| x.print());
+        print!(" ");
+        status.iter().for_each(|x| x.print());
+        print!("\n");
+        if success {
+            println!("CORRECT {}", times);
+            return;
+        }
+        times += 1;
+    }
+    println!("FAILED {}", ans_str.to_uppercase());
+}
+
+fn count_times(arr: &[usize; 5]) -> [i32; 26] {
+    let mut res = [0; 26];
+    for i in 0..5usize {
+        res[arr[i]] += 1;
+    }
+    res
+}
---
+++ src/utils.rs
@@ -0,0 +1,61 @@
+use crate::builtin_words::ACCEPTABLE;
+
+
+
+pub fn str2arr(str: &String) -> [usize; 5] {
+    assert_eq!(str.len(), 5);
+    let mut arr = [0; 5];
+    for (i, char) in str.chars().enumerate() {
+        arr[i] = cha2num(char);
+    }
+    arr
+}
+
+pub fn cha2num(cha: char) -> usize {
+    let num = cha as i32 - b'a' as i32;
+    assert!(num >= 0 && num < 26);
+    num as usize
+}
+
+pub fn valid(input: &String) -> bool {
+    if input.len() != 5 {
+        return false;
+    }
+    for ch in input.chars() {
+        if !ch.is_ascii_lowercase() {
+            return false;
+        }
+    }
+    return ACCEPTABLE.contains(&input.as_str());
+}
+
+#[derive(Clone, Copy, Debug)]
+pub enum Status {
+    G,
+    Y,
+    R,
+    X
+}
+
+impl Status {
+    pub fn update(&mut self, new: Status) {
+        match (&self, new) {
+            (Status::Y, Status::G) | 
+            (Status::R, Status::G) |
+            (Status::X, Status::G) |
+            (Status::R, Status::Y) |
+            (Status::X, Status::Y) |
+            (Status::X, Status::R) => *self = new,
+            (_, _) => (),
+        }
+    }
+
+    pub fn print(&self) {
+        match &self {
+            Status::G => print!("G"),
+            Status::Y => print!("Y"),
+            Status::R => print!("R"),
+            Status::X => print!("X"),
+        }
+    }
+}
